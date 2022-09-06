f51e4044ea021b00fe937382079f7094b139b4c9
s-bogelund <simbj94@gmail.com>
2022-09-06 11:46:07 +0200

    Fix to api paths

--- todo-backend/Program.cs
+++ todo-backend/Program.cs
@@ -6,11 +6,25 @@ using todo_backend;
 using todo_backend.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: MyAllowSpecificOrigins,
+					  policy =>
+					  {
+						  policy.WithOrigins("http://localhost:5173",
+											  "http://www.contoso.com")
+												.AllowAnyHeader()
+												.AllowAnyMethod();
+					  });
+});
 
 var app = builder.Build();
 
@@ -20,6 +34,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
+
 
 app.UseHttpsRedirection();
 
@@ -38,11 +55,11 @@ app.MapGet("/seedLists", () =>
 app.MapGet("/todoitems", async () =>
     await db.TodoList.ToListAsync());
 
-app.MapGet("/todoitems/{id}", async (int todoId) =>
+app.MapGet("/todoitems/{todoId}", async (int todoId) =>
 await db.TodoList.FindAsync(todoId) 
-is TodoList todoList 
-? Results.Ok(todoList) 
-: Results.NotFound());
+	is TodoList todoList 
+	? Results.Ok(todoList) 
+	: Results.NotFound());
 
 app.MapGet("/tasks", async () =>
     await db.TodoTask.ToListAsync());
