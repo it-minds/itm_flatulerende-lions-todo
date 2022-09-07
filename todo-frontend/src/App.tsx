e2c902a137c9137c3871de050e2baa1a41a9cfd7
s-bogelund <simbj94@gmail.com>
2022-09-07 15:36:22 +0200

    Finished Modal0.1

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -1,29 +1,14 @@
 import { useState } from "react";
 import "./index.css";
 import reactLogo from "./assets/react.svg";
-import {
-	getSubTasks,
-	getTasks,
-	getTodo,
-	getTodos,
-	getTodoTasks,
-} from "./utils/getRequests";
-import {
-	addSubTask,
-	addTask,
-	addTodoList,
-	TEST_SUBTASK,
-	TEST_TASK,
-	TEST_TODO,
-} from "./utils/postRequests";
-import { updateSubTask, updateTask, updateTodoList } from "./utils/putRequests";
-import { deleteTodoList } from "./utils/deleteRequest";
+import Modal from "./components/Modal";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const [modalOpen, setModalOpen] = useState(false);
 
 	return (
-		<div className="bg">
+		<div className="app-root bg">
 			<div>
 				<a href="https://vitejs.dev" target="_blank">
 					<img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -34,7 +19,7 @@ function App() {
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card bg-slate-200">
-				<button onClick={() => deleteTodoList(4)}>Get Todos!</button>
+				<button onClick={() => setModalOpen(true)}>Open Modal!</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
@@ -42,6 +27,16 @@ function App() {
 			<p className="read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
+			<Modal
+				isOpen={modalOpen}
+				onClose={() => {
+					console.log("closing modal");
+					setModalOpen(false);
+				}}
+				className="fade"
+			>
+				<h1>Modal</h1>
+			</Modal>
 		</div>
 	);
 }
