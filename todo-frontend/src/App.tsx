4eab75c55e197a3a58dfeedf91449879422108d0
MadsBorkNees <madsbork@live.dk>
2022-09-08 08:58:30 +0200

    small test change

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
 				</p>
 			</div>
 			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
+				Click on the Vite and React logos to learn more - Heeeeeej
 			</p>
 			<nav>
 				<Link to="/">Home</Link>
