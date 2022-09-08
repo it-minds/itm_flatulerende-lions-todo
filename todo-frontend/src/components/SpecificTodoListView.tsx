bcb805bafa54f49020b315bb65c43efc91688b40
MadsBorkNees <madsbork@live.dk>
2022-09-08 14:32:47 +0200

    Added task fetch call and loading for todo-lists

--- todo-frontend/src/components/SpecificTodoListView.tsx
+++ todo-frontend/src/components/SpecificTodoListView.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 import { getTasksDummy } from '../dummy-data';
+import { useFetch } from '../Hooks/useFetch';
+import BASE_PATH from '../utils/getRequests';
+import { TodoList, TodoTask } from '../utils/todoTypes';
 import CompressedTask from './CompressedTask';
 
 // Skal rimelig sikkert have en prop med der er et todo-liste-objekt
@@ -7,13 +11,17 @@ import CompressedTask from './CompressedTask';
 // (Det er sådan det er sat op i backend)
 
 const SpecificTodoListView = () => {
-    let tasks = getTasksDummy();
+    
+    const {todoListId} = useParams();    
+
+    const {data, loading, error} = useFetch<TodoTask[]>({url: `${BASE_PATH}/tasks/${todoListId}`});
 
 	return (
 		<div className="bg-brown-gray box-border min-h-fit p-4 border-4 w-[95%] md:w-3/4 rounded-xl ">
+            {loading && <div className="text-white flex items-center justify-center p-3">Loading......</div>}
 			<div className="flex justify-center">
 				<div className="w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-					{tasks.map((task) => (
+                    {data && data?.map((task) => (
 						<CompressedTask task={task}/>
 					))}
 				</div>
