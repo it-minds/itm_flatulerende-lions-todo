366589da298d917f2256a3f5c1dd232b8479fa93
MadsBorkNees <madsbork@live.dk>
2022-09-08 10:17:37 +0200

    Reordered CompressedTodoList and added deletion confirmation

--- todo-frontend/src/components/CompressedTodoList.tsx
+++ todo-frontend/src/components/CompressedTodoList.tsx
@@ -30,23 +30,26 @@ const CompressedTodoList:FC<Props> = ({todoList}) => {
         console.log(checkBoxState);
     }
 
-    function handleDeleteIconClick() {
-        console.log(`You deleted list #${todoList.todoListId}`);
-    }
+    const handleDeleteIconClick = async () => {
+        const result = await confirm(`Are you sure you want to delete ${todoList.todoListName} (Id: ${todoList.todoListId})?`);
+        if (result) {
+          console.log("Deletion confirmed");
+          return;
+        }
+        console.log("Deletion cancelled");
+      };
 
   return (
     <div className="m-1 py-2 px-4 font-medium text-left text-black rounded-lg bg-zinc-50 hover:bg-zinc-100 active:bg-zinc-200 border-b border-gray-500 cursor-pointer "
         >
         <div className="grid grid-cols-11 gap-4">
-            <label>
-            Complete: <input type="checkbox" checked={checkBoxState} onChange={handleCheckBoxClick}/>
+            <label className='col-span-2'>
+                Complete: <input type="checkbox" checked={checkBoxState} onChange={handleCheckBoxClick}/>
             </label>
-            {/* <div className="col-span-1">{todoList.todoListId}</div> */}
             <div className="col-span-3">{todoList.todoListName}</div>
             <div className="col-span-3">{todoList.todoListDesc}</div>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded col-span-2" onClick={handleGotoButtonClick}>Go to list.</button>
-            <div className="col-span-1 content-center" onClick={handleDeleteIconClick}><TbTrash /></div>
-            <div className="col-span-1">{deletionStatus()}</div>
+            <div className="col-span-1 content-around ml-2" onClick={handleDeleteIconClick}><TbTrash size={35}/></div>
         </div>
     </div>
   )
