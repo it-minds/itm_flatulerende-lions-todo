8d66cbb5e99c6e1a1759c1b9fdc7985c605f016b
MadsBorkNees <madsbork@live.dk>
2022-09-08 09:58:33 +0200

    Added clickable button and delete icon

--- todo-frontend/src/components/CompressedTodoList.tsx
+++ todo-frontend/src/components/CompressedTodoList.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { TodoListModel } from '../Models/TodoListModel'
+import { TbTrash } from 'react-icons/tb';
 
 type Props = {
     todoList: TodoListModel
@@ -9,6 +10,7 @@ type Props = {
 const CompressedTodoList:FC<Props> = ({todoList}) => {
 
     const navigate = useNavigate();
+    const [checkBoxState, setCheckBoxState] = useState(false);
 
     const deletionStatus = () => {
         if (!todoList.todoListDeleted) {
@@ -19,17 +21,31 @@ const CompressedTodoList:FC<Props> = ({todoList}) => {
         }
     }
 
-    function handleClick() {
+    function handleGotoButtonClick() {
         navigate(`/TodoList/${todoList.todoListId}`);
+    }
+
+    function handleCheckBoxClick() {
+        setCheckBoxState(!checkBoxState);
+        console.log(checkBoxState);
+    }
+
+    function handleDeleteIconClick() {
+        console.log(`You deleted list #${todoList.todoListId}`);
     }
 
   return (
     <div className="m-1 py-2 px-4 font-medium text-left text-black rounded-lg bg-zinc-50 hover:bg-zinc-100 active:bg-zinc-200 border-b border-gray-500 cursor-pointer "
-        onClick={handleClick}>
-        <div className="grid grid-cols-8 gap-4">
-            <div className="col-span-1">{todoList.todoListId}</div>
+        >
+        <div className="grid grid-cols-11 gap-4">
+            <label>
+            Complete: <input type="checkbox" checked={checkBoxState} onChange={handleCheckBoxClick}/>
+            </label>
+            {/* <div className="col-span-1">{todoList.todoListId}</div> */}
             <div className="col-span-3">{todoList.todoListName}</div>
             <div className="col-span-3">{todoList.todoListDesc}</div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded col-span-2" onClick={handleGotoButtonClick}>Go to list.</button>
+            <div className="col-span-1 content-center" onClick={handleDeleteIconClick}><TbTrash /></div>
             <div className="col-span-1">{deletionStatus()}</div>
         </div>
     </div>
--- todo-frontend/src/components/HomepageTodoListView.tsx
+++ todo-frontend/src/components/HomepageTodoListView.tsx
@@ -6,7 +6,7 @@ const HomepageTodoListView = () => {
 	let todoLists = getTodoLists();
 
 	return (
-		<div className="bg-brown-gray box-border min-h-fit p-4 border-4  w-3/4 rounded-xl">
+		<div className="bg-brown-gray box-border min-h-fit p-4 border-4 w-[95%] md:w-3/4 rounded-xl ">
 			<div className="flex justify-center">
 				<div className="w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
 					{todoLists.map((todoList) => (
