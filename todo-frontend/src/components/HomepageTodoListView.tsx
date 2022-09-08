d041dc574ce86c3bff8a08ae95e75eb21a7cb907
MadsBorkNees <madsbork@live.dk>
2022-09-08 14:02:41 +0200

    Added loading spinner to homepage

--- todo-frontend/src/components/HomepageTodoListView.tsx
+++ todo-frontend/src/components/HomepageTodoListView.tsx
@@ -11,6 +11,7 @@ const HomepageTodoListView = () => {
 
 	return (
 		<div className="bg-brown-gray box-border min-h-fit p-4 border-4 w-[95%] md:w-3/4 rounded-xl ">
+			{loading && <div className="text-white flex items-center justify-center p-3">Loading......</div>}
 			<div className="flex justify-center">
 				<div className="w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
 					{data && data?.map((todoList) => (
