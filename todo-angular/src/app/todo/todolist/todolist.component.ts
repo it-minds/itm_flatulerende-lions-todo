a1104cdd75bcf9761db4daa81a38e576d0e38f41
s-bogelund <simbj94@gmail.com>
2022-09-12 15:55:10 +0200

    Renders todolists received from the backend

--- todo-angular/src/app/todo/todolist/todolist.component.ts
+++ todo-angular/src/app/todo/todolist/todolist.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoModel } from 'src/Models/TodoModel';
+import { TodoService } from './../todo.service';
 
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.scss']
+  styleUrls: ['./todolist.component.scss'],
 })
 export class TodolistComponent implements OnInit {
+  todoList: Array<TodoModel>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly todoService: TodoService) {
+    this.todoList = new Array<TodoModel>();
   }
 
+  getTodos() {
+    this.todoService.getTodos().subscribe({
+      next: (todos) => {
+        this.todoList = todos;
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.getTodos();
+  }
 }
