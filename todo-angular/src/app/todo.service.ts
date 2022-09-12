91b2b7b06edc5dbb7aa3ff337164b5b3d1d6ac79
s-bogelund <simbj94@gmail.com>
2022-09-12 13:18:52 +0200

    Added http get request to todo-service to get all todo lists from backend

--- todo-angular/src/app/todo.service.ts
+++ todo-angular/src/app/todo.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@angular/core';
 import { TodoComponent } from './todo/todo.component';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  constructor(todo: TodoComponent) {}
+  constructor(private http: HttpClient) {}
 
-  getTodos() {}
+  private todosUrl = 'https://localhost:7277';
+
+  getTodos(): Observable<TodoComponent[]> {
+    return this.http
+      .get<TodoComponent[]>(`${this.todosUrl}/todoitems`)
+      .pipe(catchError(this.handleError<TodoComponent[]>('getTodos', [])));
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
--- todo-angular/src/app/todo/todo.component.ts
+++ todo-angular/src/app/todo/todo.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  constructor(private readonly todoService: TodoService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getTodos() {
+    this.todoService.getTodos().subscribe({
+      next: (todos) => {
+        console.log(todos);
+      },
+    });
   }
-
 }
