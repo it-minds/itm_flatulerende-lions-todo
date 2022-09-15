3161c4fbdd984eeae70efd92d91f1c1018c7dbcc
s-bogelund <simbj94@gmail.com>
2022-09-15 18:26:32 +0200

    TodoList responsiveness changes, accordion no longer exapnds if no subtasks

--- todo-angular/src/app/todo/todolist/todolist.component.ts
+++ todo-angular/src/app/todo/todolist/todolist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AddTaskComponent } from '../task/add-task/add-task.component';
 import { TaskModel, TodoModel } from 'src/Models/TodoModel';
@@ -13,7 +13,9 @@ import { TodoService } from './../todo.service';
 export class TodolistComponent implements OnInit {
   todoListTasks: TaskModel[] = [];
   todoListId: string | null = '';
-
+  getScreenWidth!: number;
+  mdScreenSize: number = 768;
+  noSubTasks: boolean = false;
   panelOpenState = false; // Panel state (From Angular Materials)
 
   constructor(
@@ -23,8 +25,9 @@ export class TodolistComponent implements OnInit {
 
   ngOnInit(): void {
     this.todoListId = this.route.snapshot.paramMap.get(TODO_LIST_ID); // Læser parameteren "TODO_LIST_ID" fra URL'en (sat i todo-routing module)
-
+    this.getScreenWidth = window.innerWidth;
     this.getSpecificTodos(this.todoListId);
+    if (this.todoListTasks.length === 0) this.noSubTasks = true;
   }
 
   onNewTaskAdded(newTask: TaskModel) {
@@ -41,6 +44,10 @@ export class TodolistComponent implements OnInit {
     });
   }
 
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+  }
   onCheckboxChanged(task: TaskModel) {
     task.taskComplete = !task.taskComplete;
     this.todoService.updateTask(task.todoTaskId, task).subscribe({
